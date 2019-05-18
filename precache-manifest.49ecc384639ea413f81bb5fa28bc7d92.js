49bd4e8ad2b8c23ad9d74c0f8e6ac3fd5e28a89a
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-05-18 15:29:37 +0200

    Updates

---
+++ precache-manifest.49ecc384639ea413f81bb5fa28bc7d92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2360b76c7cf899ccb59f43b87bb4229f",
+    "url": "/react-album-with-photos/index.html"
+  },
+  {
+    "revision": "05e8102eb44b2d8228a8",
+    "url": "/react-album-with-photos/static/css/main.b2e453a2.chunk.css"
+  },
+  {
+    "revision": "cc31cc5c53cd433b7ff1",
+    "url": "/react-album-with-photos/static/js/2.c5669b0c.chunk.js"
+  },
+  {
+    "revision": "05e8102eb44b2d8228a8",
+    "url": "/react-album-with-photos/static/js/main.88ee93cf.chunk.js"
+  },
+  {
+    "revision": "94a25595de7640b5d569",
+    "url": "/react-album-with-photos/static/js/runtime~main.e468e09d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-album-with-photos/precache-manifest.cd7b674ada73a7cc0f7da62d9c7a1544.js"
+  "/react-album-with-photos/precache-manifest.49ecc384639ea413f81bb5fa28bc7d92.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-album-with-photos/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-album-with-photos/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
