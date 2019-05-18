a885ef2c493043fd877ab991713b0cf8d7a7408d
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-05-18 15:43:38 +0200

    Change router to ash router for gh-pages

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,7 +4,7 @@ import {header, heading1} from './Layout.module.scss';
 
 const Header = () => (
   <nav className={header}>
-      <h1 className={heading1}>Test Assignment</h1>
+      <h1 className={heading1}>React Album App</h1>
   </nav>
 );
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 
 // Layout view-components
 import Header from '../components/Layout/Header';
